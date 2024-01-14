9f7d824cc307bf990afdd8cde73565bf1be4ec19
Onkar Divekar <onkardivekar07@gmail.com>
2024-01-14 10:25:37 +0530

    task 8

--- app.js
+++ app.js
@@ -4,34 +4,45 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
-const User = require("./models/user");
-
+//const mongoConnect = require("./util/database").mongoConnect;
+//const User = require("./models/user");
+const mongoose = require("mongoose");
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+//const adminRoutes = require("./routes/admin");
+//const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("5baa2528563f16379fc8a610")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("5baa2528563f16379fc8a610")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+//app.use("/admin", adminRoutes);
+//app.use(shopRoutes);
 
-app.use(errorController.get404);
+//app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    "mongodb+srv://OnkarDivekkar:121021@cluster0.vsl4e0p.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    //console.log(result);
+    app.listen(3000);
+    console.log(`Server Started && connection with database established`);
+  })
+  .catch((err) => {
+    {
+      console.log(err);
+    }
+  });
